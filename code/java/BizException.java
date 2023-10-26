67001b0e3d2644201b4044055b62bfe2c7bd7b97
dlyss <dongliangyuss@163.com>
2023-10-26 22:22:18 +0800

    Create BizException.java

---
+++ code/java/BizException.java
@@ -0,0 +1,25 @@
+@Getter
+@AllArgsConstructor
+public class BizException extends RuntimeException {
+
+    /**
+     * 错误码
+     */
+    private Integer code = FAIL.getCode();
+
+    /**
+     * 错误信息
+     */
+    private final String message;
+
+    public BizException(String message) {
+        this.message = message;
+    }
+
+    public BizException(StatusCodeEnum statusCodeEnum) {
+        this.code = statusCodeEnum.getCode();
+        this.message = statusCodeEnum.getDesc();
+    }
+
+
+}
